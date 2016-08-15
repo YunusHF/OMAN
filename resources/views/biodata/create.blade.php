c71a268374dac0573888940e050b80adba22dfd0
denni anggrianto <denni.anggrianto17@gmail.com>
2016-08-15 13:22:08 +0700

    Jajal_push

--- resources/views/biodata/create.blade.php
+++ resources/views/biodata/create.blade.php
@@ -1,75 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('/.layouts.tampilan')
 
-    <head>
-        
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>SI OMAN</title>
-
-        <!-- CSS -->
-        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,400">
-        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Droid+Sans">
-        <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="../assets/font-awesome/css/font-awesome.min.css">
-        <link rel="stylesheet" href="../assets/css/animate.css">
-        <link rel="stylesheet" href="../assets/css/style.css">
-        <link rel="stylesheet" href="../assets/css/media-queries.css">
-        <link rel="stylesheet" href="../assets/css/jquery-ui.css">
-        <script src="assets/js/jquery-1.11.1.min.js"></script>
-        <script src="assets/js/jquery-ui.js"></script>
-        <script type="text/javascript">
-	        $(function () {
-	            $('#datetimepicker1').datetimepicker();
-	        });
-		</script>
-
-
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-        <![endif]-->
-
-        <!-- Favicon and touch icons -->
-        <link rel="shortcut icon" href="assets/ico/favicon.ico">
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
-
-    </head>
-
-    <body>
-        
-        <!-- Top menu -->
-		<nav class="navbar" role="navigation">
-			<div class="container">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#top-navbar-1">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="index.html">Office Management PT. Nufaza</a>
-				</div>
-				<!-- Collect the nav links, forms, and other content for toggling -->
-				<div class="collapse navbar-collapse" id="top-navbar-1">
-					<ul class="nav navbar-nav navbar-right">
-						<li>
-							<a href="portfolio.html"><i class="fa fa-user"></i><br>Profil Karyawan</a>
-						</li>
-						<li>
-							<a href="services.html"><i class="fa fa-tasks"></i><br>Proyek Yang Dikerjakan</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-
+@section('konten')
 		<!-- Form Login-->
 		 <div class="presentation-container">
 	        <div class="container">
@@ -232,7 +163,7 @@
 	            						<div class="col-md-2"><p>Tanggal Mulai Kerja</p></div>
 			            				<div class="col-md-4"><p>
 			            					<div class='input-group date' id='datetimepicker1'>
-						                    <input type="text" name="tangal_mulai_kerja">
+						                    <input type="text" name="tanggal_mulai_kerja">
 						                    <span class="input-group-addon">
 						                        <span class="glyphicon glyphicon-calendar"></span>
 						                    </span>
@@ -300,18 +231,6 @@
 	            </div>
 	        </div>
 	     </div>
+@endsection
 
-        <!-- Javascript -->
-
-        <script src="assets/js/jquery-1.11.1.min.js"></script>
-        <script src="assets/js/jquery-ui.js"></script>
-        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-        <script src="assets/js/bootstrap-hover-dropdown.min.js"></script>
-        <script src="assets/js/wow.min.js"></script>
-        <script src="assets/js/retina-1.1.0.min.js"></script>
-        <script src="assets/js/scripts.js"></script>
-
-    </body>
-
-</html>
-
+     
---
+++ resources/views/biodata/tampil.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.tampilan')
+  
+  @section('konten')  	
+    	<div class="services-container">
+	        <div class="container">
+	            <div class="row">
+	            	<div class="col-md-12">
+	            		<div id="user-biodata">
+	            			<div class="user-summary">
+	            				<div class="container">
+	            					<div class="row">
+	            						<div class="col-md-3 col-sm-12">
+	            							<img src="../assets/img/dde.jpeg">
+	            						</div>
+	            						@foreach($datadiri as $datadiri)
+	            						<div class="col-md-9">
+	            							<div class="col-md-12 col-sm-12" ><h1><strong>{{$datadiri->nama}}</strong></h1></div>
+	            							<div class="col-md-9 col-sm-12"><h3>Desainer Basis Data</h3></div>
+	            							<div class="col-md-3 col-sm-12"><h3>Desainer Basis Data</h3></div>
+	            						</div>
+	            					</div>
+	            				</div>
+	            			</div>
+	            			<div class="user-data">
+	            				<div class="container">
+	            					<div class="row">
+	            						<div class="col-md-9">
+				            				<div class="col-md-5 col-sm-5"><p>Nama Lengkap</p></div><div class="col-md-1"><p>:</p></div><div class="col-md-6 col-sm-6"><p>{{$datadiri->nama}}</p></div>
+				            			</div>
+				            			<div class="col-md-9">
+				            				<div class="col-md-5"><p>Tempat dan Tanggal Lahir</p></div><div class="col-md-1"><p>:</p></div><div class="col-md-6"><p>{{$datadiri->tempat_lahir}}, {{$datadiri->tanggal_lahir}}</p></div>
+				            			</div>
+				            			<div class="col-md-9">
+				            				<div class="col-md-5"><p>Alamat</p></div><div class="col-md-1"><p>:</p></div><div class="col-md-6"><p>{{$datadiri->alamat}}</p></div>
+				            			</div>
+				            			<div class="col-md-9">
+				            				<div class="col-md-5"><p>Nomor Handphone</p></div><div class="col-md-1"><p>:</p></div><div class="col-md-6"><p>{{$datadiri->no_hp}}</p></div>
+				            			</div>
+				            			<div class="col-md-9">
+				            				<div class="col-md-5"><p>Email</p></div><div class="col-md-1"><p>:</p></div><div class="col-md-6"><p>{{$datadiri->email}}</p></div>
+				            			</div>
+				            			<div class="col-md-9">
+				            				<div class="col-md-5"><p>Status</p></div><div class="col-md-1"><p>:</p></div><div class="col-md-6"><p>{{$datadiri->status_perkawinan}}</p></div>	
+				            			</div>		
+				            			<!-- <div class="col-md-9">
+				            				<div class="col-md-5"><p>Istri</p></div><div class="col-md-1"><p>:</p></div><div class="col-md-6"><p>Milladina Noer Hanifah</p></div>
+				            			</div>
+				            			<div class="col-md-9">
+				            				<div class="col-md-5"><p>Anak</p></div><div class="col-md-1"><p>:</p></div><div class="col-md-6"><p>Azinudin Achzab</p><p>Raka Pratama Tiyarno</p><p>Muhammad Rasyid Hidayat</p></div>		            			
+					            		</div> -->
+					            		@endforeach
+					            	</div>
+					            </div>
+		            		</div>
+	            		</div>
+	            	</div>
+	            </div>
+	        </div>
+	     </div>
+@endsection
+
+	 
